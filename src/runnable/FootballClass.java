e8e02180a2206097b0e5c2db6a78b181d869b686
Akin Urkumbaev <akinzza@Akins-MacBook-Pro.local>
2019-06-01 13:22:03 -0500

    football commit

---
+++ src/runnable/FootballClass.java
@@ -0,0 +1,14 @@
+package runnable;
+
+public class FootballClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Today is UCL final");
+		
+		System.out.println("Tottenham - Liverpool");
+		
+		System.out.println("Liverpool is going to win");
+	}
+
+}
